028dba5c15c1f54a59a69bc7455cb148b6271260
bix <840573+bix6@users.noreply.github.com>
2020-04-06 09:08:38 -0700

    organize; modularize and layer

---
+++ src/store.js
@@ -0,0 +1,13 @@
+const cards = [{
+    id: 1,
+    title: 'Task One',
+    content: 'Card One'
+}];
+
+const lists = [{
+    id: 1,
+    header: 'List One',
+    cardIds: [1]
+}];
+
+module.exports = { cards, lists };
